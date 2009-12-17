ca0a889d3ffd54d1648431767a6631d42d1d5bc6
und3f <d3fin3@gmail.com>
2009-12-17 11:00:35 +0200

    Mojolicious generated

---
+++ lib/Accountious/Example.pm
@@ -0,0 +1,16 @@
+package Accountious::Example;
+
+use strict;
+use warnings;
+
+use base 'Mojolicious::Controller';
+
+# This action will render a template
+sub welcome {
+    my $self = shift;
+
+    # Render template "example/welcome.html.ep" with message
+    $self->render(message => 'Welcome to the Mojolicious Web Framework!');
+}
+
+1;
