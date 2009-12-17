2c188e1d6a7c5f39bb38bae5516871f61cd4f0e7
und3f <d3fin3@gmail.com>
2009-12-17 11:21:25 +0200

    Config added; Example removed

--- lib/Accountious.pm
+++ lib/Accountious.pm
@@ -7,16 +7,48 @@ use warnings;
 
 use base 'Mojolicious';
 
+use Mojo::JSON ();
+
+my %config = (
+    loglevel    => 'debug',
+);
+
+sub _load_config_file {
+    my ($self, $conf_file) = @_;
+    if (-e $conf_file) {
+        if (open FILE, '<', $conf_file) {
+            my @lines = <FILE>;
+            close FILE;
+            my $source = join '', map { $_=~s/\#.*$//;chomp;$_ } @lines;
+            my $json = Mojo::JSON->new;
+            my $json_config = $json->decode( $source ) or $self->log->fatal("JSON parsing failed: ", $json->error);
+            %config = (%config, %$json_config);
+        };
+    };
+};
+
 # This method will run once at server start
 sub startup {
     my $self = shift;
 
+    # Load configuration
+    $self->_load_config_file( $self->home->rel_file('accountious.conf') );
+
+    # Use latests templates
+    $self->renderer->default_handler('ep');
+
+    # And nice encoding
+    $self->renderer->encoding('utf-8');
+
+    # Logging
+    $self->log->level( $config{loglevel} );
+    $self->mode( 'production' ) if $self->log->level ne 'debug';
+    
     # Routes
     my $r = $self->routes;
 
-    # Default route
-    $r->route('/:controller/:action/:id')
-      ->to(controller => 'example', action => 'welcome', id => 1);
+    # Routes
+
 }
 
 1;
